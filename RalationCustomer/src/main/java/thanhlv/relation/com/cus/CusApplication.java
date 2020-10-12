f8f109af04bd33c219f9521c41d645ed90e80288
unknown <lethanhht.neu@gmail.com>
2020-10-12 23:58:38 +0700

    first commit

---
+++ RalationCustomer/src/main/java/thanhlv/relation/com/cus/CusApplication.java
@@ -0,0 +1,13 @@
+package thanhlv.relation.com.cus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CusApplication.class, args);
+    }
+
+}
